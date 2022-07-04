a85cd89767a44cdcc51ddd160e57e8d8fa3c4c96
Ajay <sajayy1993@gmail.com>
2022-07-04 14:08:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import {Board} from './componenets/Board'
+import { ScoreBoard } from './componenets/ScoreBoard';
+
 function App() {
+  const WIN_CONDITIONS=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  const [board, setBoard]= useState(Array(9).fill(null));
+  const [xPlaing, setxPlaying]= useState(true);
+  const [scores, setScores]=useState({xScore:0, oScore:0})
+  const [gameOver, setGameOver]=useState(false)
+
+  const handleBoxClick = (boxidx)=>{
+    const updateBoard= board.map((value,idx)=>{
+      if(idx===boxidx){
+        return xPlaing===true ? "X" : "O";
+       }else{
+         return value;
+       }
+    })
+   const winner= checkWinner(updateBoard);
+   if(winner){
+     if(winner==="O"){
+       let {oScore}=scores;
+       oScore+=1
+       setScores({...scores, oScore})
+     }else{
+      let {xScore}=scores;
+      xScore+=1
+      setScores({...scores, xScore})
+     }
+   }
+
+   
+    setBoard(updateBoard);
+    setxPlaying(!xPlaing);
+  }
+
+  const checkWinner =(board)=>{
+    for (let i=0; i<WIN_CONDITIONS.length; i++)
+{
+  const [x,y,z]=WIN_CONDITIONS[i];
+  if(board[x]&& board[x]===board[y]&&board[y]===board[z]){
+  setGameOver(true)
+    return board[x];
+  }
+}  }
+
+const resetBoard=()=>{
+  setGameOver(false);
+  setBoard(Array(9).fill(null))
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScoreBoard scores={scores} xPlaying={xPlaing}/>
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick}/>
+ 
     </div>
   );
 }
---
+++ src/componenets/Box.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Box.css'
+export const Box = ({value, onClick}) => {
+const style = value==="X"? "box X":"box O";
+
+  return (
+   <button className={style} onClick={onClick}>{value}</button>
+  )
+}
